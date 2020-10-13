394fcf2ed4d2f52a4ab287bfe30ab8b5770f9d31
israelsantana01 <israelofi2@gmail.com>
2020-10-13 18:47:14 -0300

    Create the function of save orphanages without image

--- backend/src/database/migrations/1602618643829-create_orphanages.ts
+++ backend/src/database/migrations/1602618643829-create_orphanages.ts
@@ -35,6 +35,14 @@ export class createOrphanages1602618643829 implements MigrationInterface {
                     type: 'text' 
                 },
                 {
+                    name: 'instructions',
+                    type: 'text'
+                },
+                {
+                    name: 'opening_hours',
+                    type: 'varchar'
+                },
+                {
                     name: 'open_on_weekends',
                     type: 'boolean',
                     default: false,
---
+++ backend/src/database/models/Orphanage.ts
@@ -0,0 +1,28 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('orphanages')
+export default class Orphanage {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  latitude: string;
+
+  @Column()
+  longitude: string;
+
+  @Column()
+  about: string;
+
+  @Column()
+  instructions: string;
+
+  @Column()
+  opening_hours: string;
+
+  @Column()
+  open_on_weekends: boolean;
+}
