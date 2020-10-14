166c4146c374d1148b7feb85bacb322df73bfd88
israelsantana01 <israelofi2@gmail.com>
2020-10-14 11:36:24 -0300

    Finish data validation using yup library

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -2,6 +2,8 @@
 
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
+import * as Yup from 'yup';
+
 import Orphanages from '../database/models/Orphanage';
 import orphanagesView from '../views/orphanages_view';
 
@@ -43,11 +45,12 @@ export default {
     const orphanagesRepository = getRepository(Orphanages);
 
     const requestImages = request.files as Express.Multer.File[];
+
     const images = requestImages.map(image => {
       return { path: image.filename }
     });
   
-    const orphanage = orphanagesRepository.create({
+    const data = {
       name,
       latitude,
       longitude,
@@ -56,7 +59,28 @@ export default {
       opening_hours,
       open_on_weekends,
       images
+    };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required('Nome obrigatório'),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+        })
+      )
     });
+
+    await schema.validate(data, {
+      abortEarly: false
+    });
+
+    const orphanage = orphanagesRepository.create(data);
   
     await orphanagesRepository.save(orphanage);
   
